3fef4b1ec01c979a5d5890aabf7aaa4cfb47d867
SantiagoDlrr <A01749140@tec.mx>
2025-02-19 20:36:14 -0600

    Completed page 1

--- src/app/page1/page.tsx
+++ src/app/page1/page.tsx
@@ -1,16 +1,50 @@
 import Link from "next/link"
+import Image from "next/image"
+import logo from "../humberger-svgrepo-com (3).svg"
 
 export default function Home() {
-    return (
-      <div className="bg-slate-900 p-8 flex justify-between text-xl">
-        <h2>Logo</h2>
-        <nav className="list-none flex gap-8">
-          <Link href="../">Home</Link>
-          <li>aquí va 1</li>
-          <li>Page 2</li>
-        </nav>
-      </div> 
-  
+  return (
+      <div className="min-h-screen flex flex-col items-center">
+
+      <h1 className="p-8 text-4xl font-bold text-white">Services</h1>
+
+        <div className="p-8 flex justify-center items-center gap-2">
+          <Link href="../">
+            <Image
+              src={logo}
+              width={150}
+              height={150}
+              alt="logo"
+            />
+          </Link>
+          <p className="max-w-72 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo placeat, laudantium aliquam mollitia fuga inventore quia, pariatur alias nam at quas aut facere ullam nobis fugit! Ea voluptate tempore reiciendis.</p>
+        </div>
+
+        <div className="p-8 flex justify-center items-center gap-2">
+          <p className="max-w-72 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo placeat, laudantium aliquam mollitia fuga inventore quia, pariatur alias nam at quas aut facere ullam nobis fugit! Ea voluptate tempore reiciendis.</p>
+
+          <Link href="../">
+            <Image
+              src={logo}
+              width={150}
+              height={150}
+              alt="logo"
+            />
+          </Link>
+        </div>
+
+        <div className="p-8 flex justify-center items-center gap-2">
+          <Link href="../">
+            <Image
+              src={logo}
+              width={150}
+              height={150}
+              alt="logo"
+            />
+          </Link>
+          <p className="max-w-72 text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo placeat, laudantium aliquam mollitia fuga inventore quia, pariatur alias nam at quas aut facere ullam nobis fugit! Ea voluptate tempore reiciendis.</p>
+        </div>
+      </div>
     );
   }
   
